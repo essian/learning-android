af4401300d385af60b4e262daf8af2ed245ebef7
essian <jessicampdennis@hotmail.com>
2017-01-26 17:13:03 +0000

    refactors business card app

--- BusinessCard/app/src/main/java/com/example/essian/businesscard/MainActivity.java
+++ BusinessCard/app/src/main/java/com/example/essian/businesscard/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.essian.businesscard;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void sendEmail(View view) {
+        String address = getString(R.string.email);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, address);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Enquiry");
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void dialPhoneNumber(View view) {
+        String phoneNumber = getString(R.string.phone);
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + phoneNumber));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void openMap(View view) {
+        Uri geoLocation = Uri.parse("geo:53.793898, -1.541779");
+        geoLocation = Uri.parse("geo:53.793898,-1.541779?q=53.793898,-1.541779(TC\'s alterations)");
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
 }
