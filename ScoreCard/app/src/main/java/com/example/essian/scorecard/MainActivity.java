51189ec1bfb0624e60867afb780b108428443632
essian <jessicampdennis@hotmail.com>
2017-01-30 17:00:27 +0000

    refactor

--- ScoreCard/app/src/main/java/com/example/essian/scorecard/MainActivity.java
+++ ScoreCard/app/src/main/java/com/example/essian/scorecard/MainActivity.java
@@ -11,6 +11,8 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String STATE_PLAYER1_SCORE = "player1Score";
+    static final String STATE_PLAYER2_SCORE = "player2Score";
     int player1Score = 0;
     int player2Score = 0;
 
@@ -30,27 +32,25 @@ public class MainActivity extends AppCompatActivity {
         displayPlayer2Score(player2Score);
     }
 
-    static final String STATE_PLAYER1_SCORE = "player1Score";
-    static final String STATE_PLAYER2_SCORE = "player2Score";
 
-            @Override
+    @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         // Save the user's current game state
         savedInstanceState.putInt(STATE_PLAYER1_SCORE, player1Score);
         savedInstanceState.putInt(STATE_PLAYER2_SCORE, player2Score);
 
 
-        // Always call the superclass so it can save the view hierarchy state
+        // call the superclass so it can save the view hierarchy state
         super.onSaveInstanceState(savedInstanceState);
     }
 
-    public void player1AddOne(View v){
+    public void player1AddOne(View v) {
         player1Score = player1Score + 1;
         displayPlayer1Score(player1Score);
         blinkImage(1);
     }
 
-    public void player1MinusOne(View v){
+    public void player1MinusOne(View v) {
         player1Score = player1Score - 1;
         displayPlayer1Score(player1Score);
     }
@@ -66,13 +66,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void player2AddOne(View v){
+    public void player2AddOne(View v) {
         player2Score = player2Score + 1;
         displayPlayer2Score(player2Score);
         blinkImage(1);
     }
 
-    public void player2MinusOne(View v){
+    public void player2MinusOne(View v) {
         player2Score = player2Score - 1;
         displayPlayer2Score(player2Score);
     }
@@ -99,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
         scoreView.setText(String.valueOf(score));
     }
 
-    private void displayPlayer2Score (int score) {
+    private void displayPlayer2Score(int score) {
         TextView scoreView = (TextView) findViewById(R.id.player2_score);
         scoreView.setText(String.valueOf(score));
     }
@@ -110,7 +110,7 @@ public class MainActivity extends AppCompatActivity {
         anim.setDuration(80); //You can manage the time of the blink with this parameter
         anim.setStartOffset(20);
         anim.setRepeatMode(Animation.REVERSE);
-        anim.setRepeatCount(count-1);
+        anim.setRepeatCount(count - 1);
         mainImage.startAnimation(anim);
     }
 }
